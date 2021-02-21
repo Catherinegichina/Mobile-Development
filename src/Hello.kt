fd7c860f3e45e201493278c6c79d13443b2cfdf6
Catherinegichina <catherinegichinaa@gmail.com>
2021-02-21 09:16:06 +0300

    first commit

---
+++ src/Hello.kt
@@ -0,0 +1,11 @@
+ fun main (){
+     val name = arrayOf("Kay Linda")
+     val age = arrayOf(25)
+     val weight = arrayOf(20)
+     val phonenumber = ("0702361272")
+     var kenyan= (true)
+
+
+
+
+}
